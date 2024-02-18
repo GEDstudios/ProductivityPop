138b51b0ca7492b7203bc64fafc80f78476e152f
GEDstudios <guyguyeks@gmail.com>
2024-02-18 16:48:29 +0200

    a

--- AddTaskButton.js
+++ AddTaskButton.js
@@ -9,7 +9,7 @@ class AddTaskButton {
                 isStatic: true,
 
                 render: {
-                    fillStyle: '#fff', // Sets the fill color to red
+                    fillStyle: '#11bb33', // Sets the fill color to red
                     strokeStyle: 'black', // Sets the border color to black
                     lineWidth: 1 // Sets the border width
                 },
