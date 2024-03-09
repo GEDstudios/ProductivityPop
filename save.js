0a469ce2bcdeb2641ef897a3ef02880002213c27
GEDstudios <guyguyeks@gmail.com>
2024-03-09 21:43:20 +0200

    please

--- save.js
+++ save.js
@@ -34,15 +34,6 @@ async function SignIn(event) {
             // IdP data available using getAdditionalUserInfo(result)
             // ...
             event.target.parentElement.remove();
-        }).catch((error) => {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // The email of the user's account used.
-            const email = error.customData.email;
-            // The AuthCredential type that was used.
-            const credential = GoogleAuthProvider.credentialFromError(error);
-            // ...
         });
 }
 
