dec361dc4d60865a2c4301860251586e97480d7b
GEDstudios <guyguyeks@gmail.com>
2024-03-09 21:38:05 +0200

    a

--- save.js
+++ save.js
@@ -33,7 +33,7 @@ async function SignIn(event) {
             const user = result.user;
             // IdP data available using getAdditionalUserInfo(result)
             // ...
-            event.target.parentElement.remove();
+
         }).catch((error) => {
             // Handle Errors here.
             const errorCode = error.code;
