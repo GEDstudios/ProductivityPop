6820300985ff3c602a98e5e5f1e2ba9ded73249d
GEDstudios <guyguyeks@gmail.com>
2024-03-16 00:54:49 +0200

    balllssss

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ document.addEventListener("DOMContentLoaded", event => {
     sizeButtonDivs[i].style.padding = btn.value * 1.5 + "rem";
     btn.addEventListener("click", SetNewBubbleScale);
   });
-
+  console.log("balls");
 });
 
 function ToggleZoomDiv() {
